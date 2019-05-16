359f812d9f9ae8106692c2def7d0de419f2af545
metasong <metaseed@gmail.com>
2019-05-16 12:58:06 -0500

    adjust key maps

--- pages/maps.js
+++ pages/maps.js
@@ -1,6 +1,5 @@
 
 settings.hintAlign = 'left';
-unmap('r'); // r is used for remove, use ctrl-r to reload
 unmap('u'); // u is used for undo;
 /*
  ** iteration in collection or tree
@@ -11,7 +10,8 @@ unmap('u'); // u is used for undo;
  ** [: previous
  ** t: to
  ** |: time based last used(active)
- **
+ ** >: first child of the parent, next sibling is no children
+ ** <: parent, previous sibling if no parent
  ** note:
  ** all command could be prefix with numbers to repeat.
  ** the prefix number for 'to' command is the object index in collection
@@ -21,11 +21,15 @@ keyMaps = [
     [']t', 'R', false],
     ['[t', 'E', false],
     ['{t', 'g0', true],
-    ['|t', '<Ctrl-6>', true],
+    ['|t', '<Ctrl-6>', true], // last used tab
     ['}t', 'g$', true],
     [']s', 'cs', true],
     [']h', 'D', false],
     ['[h', 'S', false],
+    ['|{', 'gT', true], // first actived tab
+    ['|}', 'gt', true], // last actived tab
+    ['|[', 'B'], //  one tab history back
+    ['|]', 'F'], //  one tab history forward
     ['[u', 'gu', true],
     ['{u', 'gU', true],
 
@@ -37,36 +41,30 @@ keyMaps = [
     // display hints
     ['dh', '<Ctrl-h>', true],
     ['dH', '<Ctrl-j>', true],
-    ['dmy', 'yma', true],
     ['dmt', 'cf', true],
     ['dt', 'gf', true],
     ['dT', 'af', true],
-    ['dyc', 'yc', true],
-    ['dyi', 'yi', true],
-    ['dyt', 'yv', true],// yank element text
-    ['dmyc', 'ymc', true],
     ['di', 'q', true], // click image or button
     ['dvs', 'zv', true], // visual element select
-
+    ['dq', 'cq', true],// query word
 
     // add
     ['aj', ';i', true], // add jquery
 
     // remove
-    ['rh', ';dh', true], // delete history older than 30 days
-    ['rb', ';db', true], // remove bookmark of this page
+    ['-h', ';dh', true], // delete history older than 30 days
+    ['-b', ';db', true], // remove bookmark of this page
 
     // search and show
-    ['sla', 'sql', true], // show last action
     ['sb', 'ob', true],
     ['sg', 'og', true],
     ['sd', 'od', true],
     ['sw', 'ow', true],
     ['sy', 'oy', true],
+    ['sla', 'sql', true], // show last action
     // open
-    ['oo', 'H', true], // open opened tab in current tab
-    ['ot', 'Q', true], // open selection with google translation
-    ['oo', 'se', true, '#11Open options'],
+    ['oh', 'H', false], // open one tab history in new tab
+    ['os', 'se', true, '#11Open settings'],
     ['oba', 'ga', true], // browser about
     ['obb', 'gb', true], // browser bookmark
     ['obc', 'gc', true], // browser cache
@@ -78,9 +76,10 @@ keyMaps = [
     ['obs', 'gs', true], // browser page source
     ['obi', 'si', true], // browser inspect
     ['om', 'sm', true], // markdown preview
+    ['oo', 'go', false], // open url in current tab
 
     // close
-    ['cd', ';j', true],
+    ['cd', ';j', true], // close download shelf
     ['c}', 'gx$', true],
     ['c{', 'gx0', true],
     ['c[', 'gxt', true],
@@ -93,19 +92,39 @@ keyMaps = [
     ['\\o', 'W', false],
 
     // copy(yank)
+    ['ypf', 'yp', true], // yank form data for post
+    ['ypi', ';cp', true], // yank proxy info
+
+    // past
+    ['pp', ';ap', true], // apply proxy info from clipboard
+    ['pf', ';pf', true], // fill form with data from yf
 
     // edit
     ['eur', 'sU', true],
     ['eut', 'su', true],
 
 
-    // undo: u
+    // undo, unset, clear
     ['uh', ';m', true], // hover
     ['uc', 'X'], // undo tab close
+
+    //reload, refresh, reset
+    ['rr', 'r', true], // ctrl-r and F5
+    ['rs', 'cS', true], // refresh scrollables
+    ['r#', 'g#', true], // reload without hash
+    ['r?', 'g?', true], // reload without query string
+
+    // set, save
+    ['=pa', 'spa', true],
+    ['=pb', 'spb', true],
+    ['=pd', 'spd', true],
+    ['=ps', 'sps', true],
+    ['=pc', 'spc', true],
+
     // toggle
-    [';pi', '<Alt-p>', true], // pin
-    [';pr', 'cp', true], // proxy
-    [';m', '<Alt-m>', true], // mute
+    ['`pi', '<Alt-p>', true], // pin
+    ['`pr', 'cp', true], // proxy
+    ['`m', '<Alt-m>', true], // mute
 
     // mis
     ['z0', 'zr']
@@ -120,6 +139,38 @@ keyMaps.forEach(map => {
     rmap(map[0], map[1], map[2], undefined, map[3]);
 });
 
-mapkey('ds', '#1Go to scrollable element', function() {
+mapkey('ds', '#1Go to scrollable element', function () {
     Hints.create(Normal.refreshScrollableElements(), Hints.dispatchMouseClick);
+});
+Front.registerInlineQuery({
+    url: "https://api.shanbay.com/bdc/search/?word=",
+    parseResult: function (res) {
+        try {
+            res = JSON.parse(res.text);
+            var exp = res.msg;
+            if (res.data.definition) {
+                var pronunciations = [];
+                for (var reg in res.data.pronunciations) {
+                    pronunciations.push(`<div>[${reg}] ${res.data.pronunciations[reg]}</div>`);
+                    // pronunciations.push(`<div><audio src="${res.data[reg+'_audio']}" controls></audio></div>`);
+                }
+                var definition = res.data.definition.split("\n").map(function (d) {
+                    return `<li>${d}</li>`;
+                }).join("");
+                exp = `${pronunciations.join("")}<ul>${definition}</ul>`;
+            }
+            if (res.data.en_definitions) {
+                exp += "<hr/>";
+                for (var lex in res.data.en_definitions) {
+                    var sense = res.data.en_definitions[lex].map(function (s) {
+                        return `<li>${s}</li>`;
+                    }).join("");
+                    exp += `<div>${lex}</div><ul>${sense}</ul>`;
+                }
+            }
+            return exp;
+        } catch (e) {
+            return "";
+        }
+    }
 });
